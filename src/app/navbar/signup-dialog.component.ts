2d0bc2947e0c4454dd39a915ac4f6c02a9fff83d
Pedro Costa <pedronobregacosta@gmail.com>
2018-12-13 18:31:06 +0100

    adds success message after signup/oauth signup

--- src/app/navbar/signup-dialog.component.ts
+++ src/app/navbar/signup-dialog.component.ts
@@ -26,7 +26,8 @@ export class SignupDialogComponent implements OnInit {
   error: String;
   acceptedTermsAndConditions: boolean;
   termsAndConditionsError: boolean;
-  isLoading: boolean;
+  signupStatus: number;
+
 
   constructor(public fb: FormBuilder,
     public auth: AuthService,
@@ -34,13 +35,13 @@ export class SignupDialogComponent implements OnInit {
     public dialogRef: MatDialogRef<SignupDialogComponent>) { }
 
   ngOnInit() {
-
+    this.signupStatus = 0;
     const usernameControl = PasswordValidation.getUsernameControl();
     const emailControl = PasswordValidation.getEmailControl();
     const passwordControl = PasswordValidation.getPasswordControl();
     const confirmPasswordControl = PasswordValidation.getPasswordControl();
     const termsAndConditionsControl = PasswordValidation.getTermsAndConditionsControl();
-    this.isLoading = false;
+
     this.signupForm = this.fb.group({
       'username': usernameControl,
       'email': emailControl,
@@ -53,24 +54,33 @@ export class SignupDialogComponent implements OnInit {
   }
 
   signup() {
-    this.isLoading = true;
-    if (!this.termsAndConditions.value){
+
+    if (!this.termsAndConditions.value) {
       this.termsAndConditionsError = true;
     } else if (this.signupForm.valid) {
       this.auth.emailSignUp(this.email.value, this.password.value)
         .then((res) => {
-          this.isLoading = false;
           this.afterSignedUp(res);
-         })
-        .catch(err => {  
-          this.isLoading = false;
-          this.handleError(err); } );
+        })
+        .catch(err => {
+          this.handleError(err);
+        });
     }
   }
 
   public afterSignedUp(e) {
+    this.signupStatus = 1;
+  }
+
+  public afterOAuthSignedUp(e) {
+    this.signupStatus = 2;
+    setTimeout(function () {
+         this.close();
+  }.bind(this), 3000);
+  }
+
+  close() {
     this.dialogRef.close();
-    this.router.navigate(['dashboard']);
   }
 
   private handleError(err) {
